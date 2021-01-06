13b9170f0acdc2ea8d321cf08723c5c803fc16e5
atharva naik <atharvanaik2018@gmail.com>
2021-01-06 05:36:05 +0530

    generate-html now also generates home page

--- maintainer-scripts/generate-html.py
+++ maintainer-scripts/generate-html.py
@@ -3,6 +3,7 @@ import json
 import argparse
 from colors import color
 from pprint import pprint
+from inflect import engine
 from datetime import datetime
 
 
@@ -26,15 +27,18 @@ def render(template, context):
     return template.strip('\n').strip()
 
 
-print(f"reading templates from {TEMPLATES} ...")
+print(
+    color(f"reading templates from {TEMPLATES} ...", fg='yellow', style='bold'))
 tag = open(f"{TEMPLATES}/tag.html").read()
 post = open(f"{TEMPLATES}/post.html").read()
 feed = open(f"{TEMPLATES}/feed.html").read()
+index = open(f"{TEMPLATES}/index.html").read()
 post_excerpt = open(f"{TEMPLATES}/post_excerpt.html").read()
 post_preview = open(f"{TEMPLATES}/post_preview.html").read()
 map = json.load(open("map.json", "r"))
 
-print(f"reading raw post data from {RAW} ...")
+print(
+    color(f"reading raw post data from {RAW} ...", fg='yellow', style='bold'))
 contexts = {}
 feeds = {}
 for item in os.listdir(RAW):
@@ -49,7 +53,7 @@ for k in contexts:
     if int(contexts[k]['NEXT_POST']) > 0:
         contexts[k]['NEXT_POST_URL'] = contexts[contexts[k]
                                                 ['NEXT_POST']]['POST_URL']
-        print(contexts[k]['NEXT_POST_URL'])
+        # print(contexts[k]['NEXT_POST_URL'])
         contexts[k]['NEXT_POST_TITLE'] = contexts[contexts[k]
                                                   ['NEXT_POST']]['TITLE']
     else:
@@ -59,7 +63,7 @@ for k in contexts:
     if int(contexts[k]['PREVIOUS_POST']) > 0:
         contexts[k]['PREVIOUS_POST_URL'] = contexts[contexts[k]
                                                     ['PREVIOUS_POST']]['POST_URL']
-        print(contexts[k]['PREVIOUS_POST_URL'])
+        # print(contexts[k]['PREVIOUS_POST_URL'])
         contexts[k]['PREVIOUS_POST_TITLE'] = contexts[contexts[k]
                                                       ['PREVIOUS_POST']]['TITLE']
     else:
@@ -97,7 +101,7 @@ for k in feeds:
     posts = [v for k, v in posts]
     context = {}
     context['PREVIEWS'] = posts[0]['PREVIEWS']
-    context['CATEGORY'] = k.title()
+    context['CATEGORY'] = engine().plural(k.title())
 
     temp = ""
     for post in posts:
@@ -107,3 +111,22 @@ for k in feeds:
     open(fname, "w").write(render(feed, context))
     print(
         color(f"html file generated at {fname} ...", fg='blue', style='bold'))
+
+all_posts = []
+for k in contexts:
+    unprintStrptimeFmt = "%Y-%m-%d %H:%M:%S.%f"
+    timestamp = datetime.strptime(
+        contexts[k]['PUBLISH_DATE'], unprintStrptimeFmt).timestamp()
+    all_posts.append((timestamp, contexts[k]))
+
+all_posts = sorted(all_posts, key=lambda x: x[0], reverse=True)
+# print(all_posts)
+
+temp = ""
+context = all_posts[0][1]
+for stamp, post in all_posts:
+    temp += render(post_excerpt, post) + '\n'
+context['EXCERPTS'] = temp
+fname = os.path.realpath("../index.html")
+print(fname)
+open(fname, "w").write(render(index, context))
